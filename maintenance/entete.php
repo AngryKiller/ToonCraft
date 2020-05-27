15e0a390bc729b2b8d14068876585b528b1a24a5
Emilien52 <40137638+Emilien52@users.noreply.github.com>
2020-05-27 23:13:09 +0200

    Fix fautes d'orthographes

--- maintenance/entete.php
+++ maintenance/entete.php
@@ -1,3 +1,3 @@
 <div class="alert alert-danger">
-    <p class="text-center"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>Le mode maintenance a été activé , seul les administrateurs on accès au site internet ! <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></p>
+    <p class="text-center"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>Le mode maintenance a été activé , seuls les administrateurs ont accès au site internet ! <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></p>
 </div>
